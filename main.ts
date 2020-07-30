2a815f26906b7969b183c5b12212140c806a1aa8
ihzi999 <68045809+ihzi999@users.noreply.github.com>
2020-07-30 14:14:07 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,95 +1,29 @@
-function vague () {
-	
+function vague (num: number) {
+    for (let d2 = 0; d2 <= 4; d2++) {
+        let led2 = 0
+        if (led.point(led2, d2)) {
+            led3 = d2 + dirc[led2]
+            if (led3 >= 4) {
+                dirc[led2] = -1
+            } else if (led3 <= 0) {
+                dirc[led2] = 5
+            }
+            led.unplot(led2, d2)
+            led.plot(led2, led3)
+            break;
+        }
+    }
 }
-let led2 = 0
-let led22 = 0
 let led3 = 0
-let led4 = 0
-let led5 = 0
-let dirc = 0
-let d2 = 1
-let d3 = 2
-let d4 = 3
-let d5 = 4
-led.plot(dirc, led2)
-led.plot(d2, led22)
-led.plot(d3, led3)
-led.plot(d4, led4)
-led.plot(d5, led5)
+let dirc: number[] = []
+dirc = [1, 1, 1, 1, -1]
+led.plot(0, 0)
+led.plot(1, 1)
+led.plot(2, 2)
+led.plot(3, 3)
+led.plot(4, 4)
 basic.forever(function () {
-    if (led2 == 0) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(dirc, led2)
-            led2 += 1
-            led.plot(dirc, led2)
-            basic.pause(100)
-        }
-    } else if (led2 == 4) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(dirc, led2)
-            led2 += -1
-            led.plot(dirc, led2)
-            basic.pause(100)
-        }
-    }
-    if (led22 == 0) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(d2, led22)
-            led22 += 1
-            led.plot(d2, led22)
-            basic.pause(100)
-        }
-    } else if (led22 == 4) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(d2, led22)
-            led22 += -1
-            led.plot(d2, led22)
-            basic.pause(100)
-        }
-    }
-    if (led3 == 0) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(d3, led3)
-            led3 += 1
-            led.plot(d3, led3)
-            basic.pause(100)
-        }
-    } else if (led3 == 4) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(d3, led3)
-            led3 += -1
-            led.plot(d3, led3)
-            basic.pause(100)
-        }
-    }
-    if (led4 == 0) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(d4, led4)
-            led4 += 1
-            led.plot(d4, led4)
-            basic.pause(100)
-        }
-    } else if (led4 == 4) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(d4, led4)
-            led4 += -1
-            led.plot(d4, led4)
-            basic.pause(100)
-        }
-    }
-    if (led5 == 0) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(d5, led5)
-            led5 += 1
-            led.plot(d5, led5)
-            basic.pause(100)
-        }
-    } else if (led5 == 4) {
-        for (let index = 0; index < 4; index++) {
-            led.unplot(d5, led5)
-            led5 += -1
-            led.plot(d5, led5)
-            basic.pause(100)
-        }
+    for (let led2 = 0; led2 <= 4; led2++) {
+        vague(led2)
     }
 })
